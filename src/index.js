c15c411cd73b354d12b3d185217fa9a96264709d
Przemysław Soliński <solinskip107@gmail.com>
2023-05-12 14:59:29 +0200

    first try

---
+++ src/index.js
@@ -0,0 +1,115 @@
+import Notiflix, { Notify } from 'notiflix';
+import _ from 'lodash';
+import axios from 'axios';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const formElement = document.querySelector('form#search-form');
+const searchInput = document.querySelector('input');
+const gallery = document.querySelector('gallery');
+const loadMoreBtn = document.querySelector('load-more');
+
+const APIKey = '28547328-93a8d2ad13e96b0d78b6f83cd';
+let page = 1;
+let totalPages;
+loadMoreBtn.setAttribute('hidden', 'hidden');
+
+formElement.addEventListener('submit', submitHandler);
+
+function submitHandler(event) {
+  event.preventDefault();
+  gallery.innerHTML = null;
+  page = 1;
+  let searchValue = searchInput.value.toLowerCase().split('').join('+');
+
+  if (searchValue === '') {
+    Notiflix.Notify.failure('Search field can`t be empty');
+  } else {
+    fetchImages(searchValue);
+  }
+}
+
+async function fetchImages(searchValue) {
+  try {
+    const response = await axios.get(`https://pixabay.com/api/`, {
+      params: {
+        key: APIKey,
+        q: searchValue,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safeSearch: true,
+        page,
+        per_page: 40,
+      },
+    });
+
+    const imagesArray = [...response.data.hits];
+    const totalHits = response.data.totalHits;
+    const totalPages = Math.ceil(totalHits / 40);
+
+    if (page === 1 && totalHits > 0) {
+      Notiflix.Notify.info(`Horray! We found ${totalHits} images.`);
+      loadMoreBtn.removeAttribute('hidden');
+    }
+
+    if (totalHits === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      loadMoreBtn.setAttribute('hidden', 'hidden');
+    }
+
+    if (page === totalPages) {
+      Notiflix.Notify.info(
+        "We're sorry, but you've reached the end of search results."
+      );
+      loadMoreBtn.setAttribute('hidden', 'hidden');
+    }
+
+    renderImages(imagesArray);
+
+    if (page > 1) {
+      const { height: cardHeight } = document
+        .querySelector('.gallery')
+        .firstElementChild.getBoundingClientRect();
+
+      window.scrollBy({
+        top: cardHeight * 2,
+        behavior: 'smooth',
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    Notiflix.Notify.failure('Sorry, something went wrong.');
+  }
+}
+
+function renderImages(images) {
+  images.forEach(image => {
+    gallery.insertAdjacentHTML(
+      'beforeend',
+      `
+    <div class="photo-card">
+    <div class="img-container">
+    <a href="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+    </a>
+    </div>
+    <div class="info">
+    <p class="info-item">
+    <b>Likes</b> ${image.likes}
+    </p>
+    <p class="info-item">
+          <b>Views</b> ${image.views}
+        </p>
+        <p class="info-item">
+          <b>Comments</b> ${image.comments}
+        </p>
+        <p class="info-item">
+          <b>Downloads</b> ${image.downloads}
+        </p>
+      </div>
+    </div>`
+    );
+  });
+  lightbox.refresh();
+}
